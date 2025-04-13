96f8361b7d91f4092da857b700d6fbc8953c434e
CarmeloMelito <melitocarmelo211@gmail.com>
2025-04-13 20:02:11 +0200

    feat:w finish films

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,48 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+//FILM
+import filmList from "../film";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [selectedGenre, setSelectedGenre] = useState("");
+  const [filteredMovies, setFilteredMovies] = useState(filmList);
+  useEffect(() => {
+    if (selectedGenre === "") {
+      setFilteredMovies(filmList);
+    } else {
+      const filtered = filmList.filter(
+        (movie) => movie.genre === selectedGenre
+      );
+      setFilteredMovies(filtered);
+    }
+  }, [selectedGenre]);
 
-  return <></>;
+  const genres = [...new Set(filmList.map((movie) => movie.genre))];
+  return (
+    <>
+      <h1> Film</h1>
+
+      <label>Trova genere: </label>
+      <select
+        value={selectedGenre}
+        onChange={(event) => setSelectedGenre(event.target.value)}
+      >
+        <option value="">Tutti i generi</option>
+        {genres.map((genre, index) => (
+          <option value={genre} key={index}>
+            {genre}
+          </option>
+        ))}
+      </select>
+
+      <ul>
+        {filteredMovies.map((movie, index) => (
+          <li key={index}>
+            {movie.title} ({movie.genre})
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default App;
